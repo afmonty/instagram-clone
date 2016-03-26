9104581d0b0c3130985e105bc97b91f7f0669d68
anna hunter <annfmonty@gmail.com>
2016-03-26 17:04:16 -0500

    drop down and add to page

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,23 @@
 var iUrl = $(".inputUrl");
 var iCaption = $(".inputCaption");
 var form = $("form");
-var picsUrl = "http://small-tiyfe.herokuapp.com/collections/a_pics;"
+var picsUrl = "http://small-tiyfe.herokuapp.com/collections/a_pics";
 var pictures = [];
+
+var getSettings = $.ajax({
+		url: picsUrl,
+		type: 'get',
+		dataType: 'json',
+		success: handleData = function(data){
+			console.log("get")
+			pictures.forEach(function(list){
+			var pic = ("<div img = src'"+list.url+"</div");
+			var com = ("div>"+list.comment+"</div");
+			$("diplay").append(pic, com);
+			})
+		}
+	});
+
 
 $( ".addButton" ).click(function () {
   if ( $( "section" ).is( ":hidden" ) ) {
@@ -20,22 +35,33 @@ function saveInput(){
 	 if	((!iUrl.val()) || (!iCaption.val())){
 			alert("please don't leave fields blank")
 		}else{
-			return pictures.push(iUrl.val(), iCaption.val());
-			 display();
-	  		}	
+			pictures.push(iUrl.val(), iCaption.val());
+	
+			var postSettings = $.ajax({
+				url: picsUrl,
+				type: 'post',
+				dataType: 'json',
+				data: pictures[0],
+				success: handleData = function(pictures) {
+				  	var container = [];
+		    		container.append(iUrl.val(), iCaption.val())
+		    		})
+				},
+			 	error: console.log("error"),
+			 	complete: display
+			 })
+		}
 	};
-	// var getSettings = {
-	// 	url: picsUrl,
-	// 	type: 'get',
-	// 	dataType: 'json',
-	// 	success: display,
-	// 	error: console.log("error")
 	
 
 function display(){
-console.log("display");
+		iUrl = !iUrl;
+		iCaption = 
+		var addpic = ("<img src ='"+pictures[0]+"'/>");
+		var addcom = ("<span>"+pictures[1]+"</span>");
+		$( ".divContainer" ).append(addpic, addcom);
 
-}
+	}
 
-//$.ajax(getSettings);
+
 
